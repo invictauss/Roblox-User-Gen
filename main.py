fcc80ea8ff646729d9e1a13f984532aca1d1a1b6
Invic <83371891+invictauss@users.noreply.github.com>
2022-04-22 15:10:29 -0300

    Update main.py

--- main.py
+++ main.py
@@ -21,27 +21,27 @@ print(f'{Fore.MAGENTA}  Made by {Fore.CYAN}Invic#1337 {Fore.MAGENTA}& {Fore.CYAN
 print(f'{Fore.MAGENTA}      Roblox User Generator')
 print(f'{Fore.WHITE}╚════════════════════════════════════╝')
 print(f'')
-print(f'{Fore.WHITE}[{Fore.MAGENTA}+{Fore.WHITE}] {Fore.MAGENTA} Starting Up . . .')
+print(f'{Fore.WHITE}[{Fore.GREEN}+{Fore.WHITE}] {Fore.MAGENTA} Loading Roblox User Generator...')
 print(f'{Fore.WHITE}'*38)
 while True:
         name = namegen()
         r = requests.get("https://api.roblox.com/users/get-by-username?username=" + name)
         a = r.text
         if a.find('Id') == -1:
-            print(f'{Fore.WHITE}[{Fore.MAGENTA}+{Fore.WHITE}] ' + name + ' is not taken! | roblox.com/register')
+            print(f'{Fore.WHITE}[{Fore.GREEN}+{Fore.WHITE}] ' + name + ' is not taken! | roblox.com/register')
             print(''*38)
             open("UserNames.txt", "a").write(name + '\n')
 
 
 
 
-            # The emojis will not work on your server, so you can remove or change it.
+            # the emojis will not work on your server, so you can remove or change it
             
-            embed = DiscordEmbed(title='New Username Sniped!', color=0x183CCD) # You can change the embed color, just change the numbers after 0x.
-            embed.set_thumbnail(url="https://media.discordapp.net/attachments/947578669986095164/947609513085784125/884966499050197052.jpg")
+            embed = DiscordEmbed(title='New Username Sniped!', color=0x183CCD) # you can change the embed color, just change the numbers after 0x
+            embed.set_thumbnail(url="") # put your thumbnail image here link format
             embed.add_embed_field(name='<:verifiedmm:947306063781326859> | Username', value=f'{name}')
             embed.add_embed_field(name='<:added:947580440640901120> | Login Account', value=f'{val}')
             embed.add_embed_field(name='<:add:947580158750105711> | Register Account', value=f'{SignUp}')
-            embed.set_footer(text='') # You can customize the embed footer just put your text in ''.
+            embed.set_footer(text='') # put your text in
             webhook.add_embed(embed)
             response = webhook.execute(remove_embeds=True)
